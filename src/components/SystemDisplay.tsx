adbbfbe99679acca6c576946912c7feba12b631d
Sudip Mondal <sudip.mondal@sprinklr.com>
2023-07-01 21:45:38 +0530

    add components for nodes

--- src/components/SystemDisplay.tsx
+++ src/components/SystemDisplay.tsx
@@ -1,11 +1,12 @@
 import {System} from "@/types/System";
 import {Typography} from "@mui/material";
+import {Node} from "@/components/nodes";
 
 type SystemDisplayProps = {
   system: System
 }
 export const SystemDisplay = ({system}: SystemDisplayProps)=>{
   return <Typography>
-    {JSON.stringify(system)}
+    {system.machines.map(machine=> <Node machine={machine} key={machine.id}/>)}
   </Typography>
 }
---
+++ src/components/nodes/MessageQueue.tsx
@@ -0,0 +1,22 @@
+export const MessageQueue = () => {
+  return <svg viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" fill="#000000">
+    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+    <g id="SVGRepo_iconCarrier"><title>queue</title>
+      <g id="Layer_2">
+        <g id="invisible_box">
+          <rect width="48" height="48" fill="none"></rect>
+        </g>
+        <g id="icons_Q2">
+          <path d="M16,36a2,2,0,0,1-2-2V6a2,2,0,0,1,2-2h0a2,2,0,0,1,2,2V34a2,2,0,0,1-2,2Z"></path>
+          <path d="M24,36a2,2,0,0,1-2-2V6a2,2,0,0,1,2-2h0a2,2,0,0,1,2,2V34a2,2,0,0,1-2,2Z"></path>
+          <path d="M32,36a2,2,0,0,1-2-2V6a2,2,0,0,1,2-2h0a2,2,0,0,1,2,2V34a2,2,0,0,1-2,2Z"></path>
+          <path
+            d="M39.7,26A2.1,2.1,0,0,0,38,28.1V40H10V28.1A2.1,2.1,0,0,0,8.3,26,2,2,0,0,0,6,28V42a2,2,0,0,0,2,2H40a2,2,0,0,0,2-2V28A2,2,0,0,0,39.7,26Z"></path>
+          <path d="M9.8,15.7,2.5,11.1c-.3-.2-.5-.1-.5.3v9.2c0,.4.2.5.5.3l7.3-4.6Q10.3,16,9.8,15.7Z"></path>
+          <path d="M45.8,15.7l-7.3-4.6c-.3-.2-.5-.1-.5.3v9.2c0,.4.2.5.5.3l7.3-4.6Q46.3,16,45.8,15.7Z"></path>
+        </g>
+      </g>
+    </g>
+  </svg>
+}
